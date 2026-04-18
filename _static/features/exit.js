4f2455feeea513060987862c90beb602dfde733c
unknown <alonw97@gmail.com>
2026-04-18 17:04:38 +0300

    bfchhjfhfjhf

--- _static/features/exit.js
+++ _static/features/exit.js
@@ -1,34 +1,39 @@
 function renderExitPage(){
     const jsxCode = `
-        function Stam(){
-            return <div></div>
-        }
+        const { useEffect } = React;
+
         function ExitPage(props){
-           if (!props.userAcceptedTerms){
-                return (
-                    <p>
-                         Thank you for your interest in this study. Unfortunately, you did not accept the terms and conditions of the study, hence you are asked to leave.<br/>
-                         Please return your hit.<br/>
-                         <a target="_blank" href="https://app.prolific.com/submissions/complete?cc=CPIYJGE6">Use this link in order to return to Prolific</a>
-                    </p>
-                )
-            }
-            if (props.endedSuccessfully){
-                return (
-                    <p>
-                        You have successfully completed the study. Thank you for your participation.<br/>
-                    <a target="_blank" href="https://app.prolific.com/submissions/complete?cc=CGIVAQNE">Use this link in order to return to Prolific</a>                        
-                    </p>
-                )
+            let redirectUrl = "";
+            let message = "";
+
+            // Logic to determine URL and Message
+            if (!props.userAcceptedTerms){
+                redirectUrl = "https://app.prolific.com/submissions/complete?cc=CPIYJGE6";
+                message = "Thank you for your interest. You did not accept the terms, please return your hit.";
+            } else if (props.endedSuccessfully){
+                redirectUrl = "https://app.prolific.com/submissions/complete?cc=CGIVAQNE";
+                message = "You have successfully completed the study. Thank you!";
             } else {
-                return (
-                    <p>
-                        Thank you for your participation in this study. Unfortunately, you did not complete the study successfully.<br/>
-                        Please return your hit.<br/>
-                    <a target="_blank" href="https://app.prolific.com/submissions/complete?cc=C898AXUN">Use this link in order to return to Prolific</a>
-                    </p>
-                )
+                redirectUrl = "https://app.prolific.com/submissions/complete?cc=C898AXUN";
+                message = "You did not complete the study successfully. Please return your hit.";
             }
+
+            // The Auto-Redirect Logic
+            useEffect(() => {
+                const timer = setTimeout(() => {
+                    window.location.href = redirectUrl;
+                }, 3000); // 3 seconds delay
+
+                return () => clearTimeout(timer); // Cleanup timer if component unmounts
+            }, [redirectUrl]);
+
+            return (
+                <p>
+                    {message}<br/>
+                    Redirecting you to Prolific in 3 seconds...<br/>
+                    <a href={redirectUrl}>Click here if you are not redirected automatically.</a>
+                </p>
+            );
         }
     `
     renderReactComponent(jsxCode, "react-root", "ExitPage", JSON.stringify(js_vars));
